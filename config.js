7c5473c1e5ee04939971691f2510cf93fa3fa95d
ndlong78 <33077621+ndlong78@users.noreply.github.com>
2025-12-06 10:23:35 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.RIASEC_CONFIG = {
+brand: {
+name: "CBB & Family",
+tagline: "Cùng con định hướng tương lai",
+logo: "logo-cbb.png"
+},
+// Đổi mật khẩu admin tại đây
+adminPassword: "cbbadmin123"
+};

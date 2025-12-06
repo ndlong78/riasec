c3ff7aa533edf924edd3f5d305e1af1109757cc7
ndlong78 <33077621+ndlong78@users.noreply.github.com>
2025-12-06 10:23:12 +0700

    Delete window.RIASEC_CONFIG = { brand: { name: "CBB & Family", tagline: "Cùng con định hướng tương lai", logo: "logo-cbb.png" },  directory

--- window.RIASEC_CONFIG = { brand: { name: "CBB & Family", tagline: "Cùng con định hướng tương lai", logo: "logo-cbb.png" }, /config.js
+++
@@ -1,9 +0,0 @@
-window.RIASEC_CONFIG = {
-brand: {
-name: "CBB & Family",
-tagline: "Cùng con định hướng tương lai",
-logo: "logo-cbb.png"
-},
-// Đổi mật khẩu admin tại đây
-adminPassword: "cbbadmin123"
-};
